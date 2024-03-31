fd4a164d43c3248bcf850cad0463943e5a238872
suyytib <152405015+suyytib@users.noreply.github.com>
2024-03-31 15:12:43 +0800

    Delete online-study_Flask directory

--- online-study_Flask/table_config.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_mail import Mail
-db=SQLAlchemy()
-migrate=Migrate()
-mail = Mail()
-# 这些代码属于数据库配置模块,但为了防止循环导入的问题发生,单独放一个文件里
